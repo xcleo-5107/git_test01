ad44c744a6bd146557e04e90b3b83c67910d1c9c
xcleo <3179549224@qq.com>
2024-12-11 23:12:40 +0800

    分支A的第二次更新

--- src/main/java/com/example/gittest01/GitTest01Application.java
+++ src/main/java/com/example/gittest01/GitTest01Application.java
@@ -2,11 +2,18 @@ package com.example.gittest01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-public class GitTest01Application {
+public class GitTest01Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GitTest01Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(GitTest01Application.class, args);
     }
--- src/main/webapp/WEB-INF/jsp/A.jsp
+++ src/main/webapp/WEB-INF/jsp/A.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: xcleo
   Date: 2024/12/11
-  Time: 23:01
+  Time: 23:11
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
